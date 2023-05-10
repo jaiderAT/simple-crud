d687cf5b6e67cd69a0dc231d1f618659cc2343e0
Jaider Alba <jaider.alba@gmail.com>
2023-05-10 09:52:08 -0300

    Initial commit

---
+++ src/main/java/com/jaideralba/internetbanking/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jaideralba.internetbanking;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InternetbankingApplication.class);
+	}
+
+}
