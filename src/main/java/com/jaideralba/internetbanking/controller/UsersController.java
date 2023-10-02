de2e82a6e5688844419e4fd7e850fac2068f912e
Jaider Alba <jaider.alba@gmail.com>
2023-10-02 20:01:12 -0300

    - Add some missing descriptions on swagger

--- src/main/java/com/jaideralba/internetbanking/controller/UsersController.java
+++ src/main/java/com/jaideralba/internetbanking/controller/UsersController.java
@@ -3,7 +3,9 @@ package com.jaideralba.internetbanking.controller;
 import com.jaideralba.internetbanking.model.UserRequest;
 import com.jaideralba.internetbanking.model.UserResponse;
 import com.jaideralba.internetbanking.service.UsersService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -13,12 +15,14 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@OpenAPIDefinition(info = @Info(title = "API para CRUD de usuários", version = "1.0"))
 public class UsersController {
 
     @Autowired
     private UsersService service;
 
     @GetMapping
+    @Operation(summary = "Retorna lista com as informações de todos usuário cadastrados")
     List<UserResponse> listAll() {
         return service.listAll();
     }
@@ -37,12 +41,13 @@ public class UsersController {
     }
 
     @PutMapping
-    @Operation(summary = "Atualiza dados de usuário existente")
+    @Operation(summary = "Atualiza os dados de um usuário existente")
     UserResponse update(@RequestBody @Valid UserRequest user) {
         return service.update(user);
     }
 
     @PatchMapping("/{id}")
+    @Operation(summary = "Atualiza parcialmente os dados de um usuário existente")
     UserResponse updatePartial(@RequestBody UserRequest user, @PathVariable Long id) {
         user.setId(id);
         return service.updatePartially(user);
