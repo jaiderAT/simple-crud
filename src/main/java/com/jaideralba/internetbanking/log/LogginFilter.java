d9491d440cac9061e0e37db230cd2dfe253b1ff6
Jaider Alba <jaider.alba@gmail.com>
2023-10-04 18:21:14 -0300

    - Add unique ID mechanism (using MDC) for easier tracking of the logs in a distributed environment

---
+++ src/main/java/com/jaideralba/internetbanking/log/LogginFilter.java
@@ -0,0 +1,38 @@
+package com.jaideralba.internetbanking.log;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class LogginFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        ContentCachingRequestWrapper requestWrapper = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper responseWrapper = new ContentCachingResponseWrapper(response);
+
+        try{
+            long startTimestamp = System.currentTimeMillis();
+
+            MDC.put("requestID",String.valueOf(startTimestamp));
+
+            filterChain.doFilter(requestWrapper, responseWrapper);
+        }
+        finally {
+            MDC.clear();
+            responseWrapper.copyBodyToResponse();
+        }
+    }
+}
